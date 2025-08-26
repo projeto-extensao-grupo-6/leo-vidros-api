12575e63c87e08145ecd8538d64c4fa1d6ff2ea2
Eduarda Dias <maria.dias@sptech.school>
2025-08-26 15:54:27 -0300

    feat(app): Adicionnado metodo para mapear role

--- src/main/java/com/project/extension/dto/usuario/UsuarioMapper.java
+++ src/main/java/com/project/extension/dto/usuario/UsuarioMapper.java
@@ -1,16 +1,32 @@
 package com.project.extension.dto.usuario;
 
+import com.project.extension.entity.Role;
 import com.project.extension.entity.Usuario;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
-
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UsuarioMapper {
 
-    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "senha", source = "senha")
+    @Mapping(target = "role", source = "role")
     Usuario toEntity(UsuarioRequestDto dto);
 
     UsuarioResponseDto toResponseDto(Usuario usuario);
+
+    default Role map(String roleName) {
+        if (roleName == null) return null;
+
+        Role role = new Role();
+        if(roleName.equalsIgnoreCase("admin")) {
+            role.setId(1);
+        } else if(roleName.equalsIgnoreCase("comum")) {
+            role.setId(2);
+        } else {
+            throw new RuntimeException("Role inválida: " + roleName);
+        }
+
+        return role;
+    }
 }
