09aa566d3854d6716c25e580190acc5a469dbe18
Eduarda Dias <maria.dias@sptech.school>
2025-08-26 15:52:24 -0300

    fix(app): removendo import desnecessario

--- src/main/java/com/project/extension/repository/RoleRepository.java
+++ src/main/java/com/project/extension/repository/RoleRepository.java
@@ -1,9 +1,7 @@
 package com.project.extension.repository;
 
-
 import com.project.extension.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
