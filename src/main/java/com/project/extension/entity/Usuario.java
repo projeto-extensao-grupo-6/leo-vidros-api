08ba9d84757810775cee5442acd751f4b258783c
Eduarda Dias <maria.dias@sptech.school>
2025-08-26 09:49:48 -0300

    fix(app): alteração de atributos em usuario

--- src/main/java/com/project/extension/entity/Usuario.java
+++ src/main/java/com/project/extension/entity/Usuario.java
@@ -1,11 +1,11 @@
 package com.project.extension.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -20,4 +20,12 @@ public class Usuario {
     private String email;
     private String cpf;
     private String senha;
+
+    @ManyToMany
+    @JoinTable(
+            name = "usuario_role",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
