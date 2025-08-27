d4455b92c99a2f3b38ce4a6363c98286ea594b49
Eduarda Dias <maria.dias@sptech.school>
2025-08-27 12:07:12 -0300

    refactor(app): Ajustando retorno de metodo no repository usuario

--- src/main/java/com/project/extension/repository/UsuarioRepository.java
+++ src/main/java/com/project/extension/repository/UsuarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Optional<String> findByEmail(@NotBlank String email);
+    Optional<Usuario> findByEmail(@NotBlank String email);
 }
