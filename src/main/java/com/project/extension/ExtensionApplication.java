369a1ee2bdea87ab50b621691d70d51f07fb843c
Eduarda Dias <maria.dias@sptech.school>
2025-09-23 10:39:58 -0300

    feat(app): Criando todo

--- src/main/java/com/project/extension/ExtensionApplication.java
+++ src/main/java/com/project/extension/ExtensionApplication.java
@@ -9,5 +9,10 @@ public class ExtensionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExtensionApplication.class, args);
 	}
-
+    // TODO:
+    // criar dto para endereço e serviço
+    // criar controller para endereço
+    // criar alocação de funcionario e fazer interagir com serviço
+    // criar mais metodos para serviço como: buscar agendamento por dia, por horario, por mes
+    // criar tabelas
 }
