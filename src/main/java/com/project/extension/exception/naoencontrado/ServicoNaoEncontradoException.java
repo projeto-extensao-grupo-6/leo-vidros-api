6ebf863f130acf491ac73e25069f5f6f1a9d3d94
Eduarda Dias <maria.dias@sptech.school>
2025-09-23 10:39:34 -0300

    feat(app): Criando exception para serviço

---
+++ src/main/java/com/project/extension/exception/naoencontrado/ServicoNaoEncontradoException.java
@@ -0,0 +1,11 @@
+package com.project.extension.exception.naoencontrado;
+
+import com.project.extension.exception.naoencontrado.base.NaoEncontradoException;
+
+public class ServicoNaoEncontradoException extends NaoEncontradoException {
+    private final static String MENSAGEM = "Serviço não encontrado";
+
+    public ServicoNaoEncontradoException() {
+        super(MENSAGEM);
+    }
+}
