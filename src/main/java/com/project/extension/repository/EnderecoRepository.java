68e9ecf4799d94627d8d9e2a1ab569a0df818e63
Eduarda Dias <maria.dias@sptech.school>
2025-09-23 10:38:59 -0300

    feat(app): Criando repository para endereço

---
+++ src/main/java/com/project/extension/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.project.extension.repository;
+
+import com.project.extension.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+}
