662be3cb5d67843338f904f6bcb71ea83449c582
Eduarda Dias <maria.dias@sptech.school>
2025-10-15 16:05:04 -0300

    feat(app): criando construtor cheio e vazio para endereco

--- src/main/java/com/project/extension/entity/Endereco.java
+++ src/main/java/com/project/extension/entity/Endereco.java
@@ -5,11 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Endereco {
 
     @Id
@@ -23,4 +25,14 @@ public class Endereco {
     private String bairro;
     private Character uf;
     private String pais;
+
+    public Endereco(String rua, String complemento, String cep, String cidade, String bairro, Character uf, String pais) {
+        this.rua = rua;
+        this.complemento = complemento;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.uf = uf;
+        this.pais = pais;
+    }
 }
