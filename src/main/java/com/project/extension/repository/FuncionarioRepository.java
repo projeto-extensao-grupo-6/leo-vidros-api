0a6f2e5d2e7db3e996d91a95b57e021d84a8730f
Eduarda Dias <maria.dias@sptech.school>
2025-10-16 16:48:03 -0300

    feat(app): criando repository para funcionario

--- src/main/java/com/project/extension/repository/FuncionarioRepository.java
+++ src/main/java/com/project/extension/repository/FuncionarioRepository.java
@@ -4,4 +4,5 @@ import com.project.extension.entity.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+    Funcionario findByTelefone(String telefone);
 }
