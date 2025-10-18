cb3ba4d56e2b13aaccbf515fd667a78f1e1ff515
Eduarda Dias <maria.dias@sptech.school>
2025-10-18 00:42:53 -0300

    refactor(app): refatorando entity tipo movimentação

--- src/main/java/com/project/extension/entity/TipoMovimentacao.java
+++ src/main/java/com/project/extension/entity/TipoMovimentacao.java
@@ -1,15 +1,13 @@
 package com.project.extension.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum TipoMovimentacao {
     ENTRADA(1),
     SAIDA(2);
 
     private final Integer tipoMovimentacao;
-    TipoMovimentacao(Integer tipoMovimentacao) {
-        this.tipoMovimentacao = tipoMovimentacao;
-    }
-
-    public Integer getTipoMovimentacao() {
-        return tipoMovimentacao;
-    }
 }
