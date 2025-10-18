3009a9ad7c649bf1f85d0463c3e02862cb3946fc
Eduarda Dias <maria.dias@sptech.school>
2025-10-18 00:42:07 -0300

    refactor(app): refatorando entity produto

--- src/main/java/com/project/extension/entity/Estoque.java
+++ src/main/java/com/project/extension/entity/Estoque.java
@@ -1,91 +1,28 @@
 package com.project.extension.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "estoque")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Estoque {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne
-    @JoinColumn(name = "fk_produto", nullable = false)
-    private Produto produto; // mapeia a fk_produto
+    @JoinColumn(name = "produto_id", nullable = false)
+    private Produto produto;
 
-    @Column(nullable = false)
     private Integer quantidade;
-
-    @Column(nullable = false)
-    private Integer reservado = 0;
-
-    @Column(length = 100)
+    private Integer reservado;
     private String localizacao;
-
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
-
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
-
-    // Construtor, getters e setters
-    public Estoque() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Produto getProduto() {
-        return produto;
-    }
-
-    public void setProduto(Produto produto) {
-        this.produto = produto;
-    }
-
-    public Integer getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(Integer quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public Integer getReservado() {
-        return reservado;
-    }
-
-    public void setReservado(Integer reservado) {
-        this.reservado = reservado;
-    }
-
-    public String getLocalizacao() {
-        return localizacao;
-    }
-
-    public void setLocalizacao(String localizacao) {
-        this.localizacao = localizacao;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
