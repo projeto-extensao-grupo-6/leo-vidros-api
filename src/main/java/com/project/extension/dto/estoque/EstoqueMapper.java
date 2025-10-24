9c142ed223ab2a5aba006bfd40544ac94bfba3df
Eduarda Dias <maria.dias@sptech.school>
2025-10-24 09:36:17 -0300

    feat(app): criando dto para estoque

---
+++ src/main/java/com/project/extension/dto/estoque/EstoqueMapper.java
@@ -0,0 +1,44 @@
+package com.project.extension.dto.estoque;
+
+import com.project.extension.dto.produto.ProdutoMapper;
+import com.project.extension.entity.Estoque;
+import com.project.extension.service.ProdutoService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class EstoqueMapper {
+
+    private final ProdutoMapper produtoMapper;
+    private final ProdutoService produtoService;
+
+    public Estoque toEntity(EstoqueRequestDto dto) {
+        if (dto == null) return null;
+
+        Estoque estoque = new Estoque(
+                dto.quantidade(),
+                dto.reservado(),
+                dto.localizacao()
+        );
+
+        estoque.setProduto(produtoService.buscarPorId(dto.produtoId()));
+
+        return estoque;
+    }
+
+
+    public EstoqueResponseDto toResponse(Estoque estoque) {
+        if (estoque == null) return null;
+
+        EstoqueResponseDto responseDto = new EstoqueResponseDto(
+                estoque.getId(),
+                estoque.getQuantidade(),
+                estoque.getReservado(),
+                estoque.getLocalizacao(),
+                produtoMapper.toResponse(estoque.getProduto())
+        );
+
+        return responseDto;
+    }
+}
---
+++ src/main/java/com/project/extension/dto/estoque/EstoqueRequestDto.java
@@ -0,0 +1,14 @@
+package com.project.extension.dto.estoque;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record EstoqueRequestDto(
+        @NotNull @Positive Integer produtoId,
+        @NotBlank String localizacao,
+        @NotNull @Positive Integer quantidade,
+        @PositiveOrZero Integer reservado
+) { }
+
