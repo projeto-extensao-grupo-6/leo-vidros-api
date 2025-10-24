1fda5eaec49efecf041d7abae579ae2218cccba9
Eduarda Dias <maria.dias@sptech.school>
2025-10-24 11:17:55 -0300

    feat(app): criando repository para historico estoque

--- src/main/java/com/project/extension/repository/HistoricoEstoqueRepository.java
+++ src/main/java/com/project/extension/repository/HistoricoEstoqueRepository.java
@@ -1,9 +1,8 @@
 package com.project.extension.repository;
 
-import com.project.extension.entity.Estoque;
 import com.project.extension.entity.HistoricoEstoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HistoricoEstoqueRepository extends JpaRepository<HistoricoEstoque, Integer> {
-    void deleteByEstoque(Estoque estoque);
+
 }
