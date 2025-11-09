8f042724b4626973ac42e926dc731859d5870af2
Eduarda Dias <maria.dias@sptech.school>
2025-11-09 16:28:37 -0300

    feat(app): adicionando no corpo da resp o status

--- src/main/java/com/project/extension/dto/solicitacao/SolicitacaoResponseDto.java
+++ src/main/java/com/project/extension/dto/solicitacao/SolicitacaoResponseDto.java
@@ -1,11 +1,14 @@
 package com.project.extension.dto.solicitacao;
 
+import com.project.extension.dto.status.StatusResponseDto;
+
 public record SolicitacaoResponseDto(
 
         Integer id,
         String nome,
         String cpf,
         String email,
-        String telefone
+        String telefone,
+        StatusResponseDto status
 ) {
 }
