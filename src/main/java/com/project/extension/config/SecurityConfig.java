eba61211d2290e472f4b182f4a713df24cf0bd54
Olivaresss <rodrigo.olivares@sptech.school>
2025-11-17 20:37:00 -0300

    Adicionando endpoint funcionario

--- src/main/java/com/project/extension/config/SecurityConfig.java
+++ src/main/java/com/project/extension/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
             "/swagger-resources/**",
             "/webjars/**",
             "/auth/login",
-            "/solicitacoes/**"
+            "/solicitacoes/**",
+            "/funcionarios/**"
     };
 
     @Bean
