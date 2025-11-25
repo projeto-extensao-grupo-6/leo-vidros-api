e66a5b08d50cd4764a3248810fa06c67885e2ebc
Eduarda Dias <maria.dias@sptech.school>
2025-11-25 10:14:10 -0300

    feat(app): create service for serviço

---
+++ src/main/java/com/project/extension/service/ServicoService.java
@@ -0,0 +1,85 @@
+package com.project.extension.service;
+
+import com.project.extension.entity.Etapa;
+import com.project.extension.entity.Servico;
+import com.project.extension.exception.naoencontrado.ServicoNaoEncontradoException;
+import com.project.extension.repository.ServicoRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class ServicoService {
+
+    private final ServicoRepository repository;
+    private final EtapaService etapaService;
+    private final LogService logService;
+
+    public Servico cadastrar(Servico servico) {
+        if (servico.getEtapa() == null) {
+            Etapa etapa = etapaService.buscarPorTipoAndEtapa("PEDIDO", "PENDENTE");
+            servico.setEtapa(etapa);
+        }
+
+        Etapa etapa = etapaService.buscarPorTipoAndEtapa("PEDIDO", servico.getEtapa().getNome());
+        servico.setEtapa(etapa);
+
+        Servico servicoSalvo = repository.save(servico);
+        log.info("Novo pedido de serviço com nome: {} registrado com sucesso!", servicoSalvo.getNome());
+        return servicoSalvo;
+    }
+
+    public Servico buscarPorId(Integer id) {
+        return repository.findById(id).orElseThrow(() -> {
+            String mensagem = String.format("Falha na busca: Serviço com ID %d não encontrado.", id);
+            logService.error(mensagem);
+            log.warn("Serviço com ID {} não encontrado", id);
+            return new ServicoNaoEncontradoException();
+        });
+    }
+
+    public List<Servico> listarPorEtapa(String nome) {
+        Etapa etapa = etapaService.buscarPorTipoAndEtapa("PEDIDO", nome);
+        return etapa != null ? repository.findAllByEtapa(etapa) : repository.findAll();
+    }
+
+    public List<Servico> listar() {
+        List<Servico> servicos = repository.findAll();
+        logService.info(String.format("Busca por todos os serviços. Total de registros: %d.", servicos.size()));
+        return servicos;
+    }
+
+    private void atualizarCampos(Servico destino, Servico origem) {
+        destino.setNome(origem.getNome());
+        destino.setCodigo(origem.getCodigo());
+        destino.setDescricao(origem.getDescricao());
+        destino.setPrecoBase(origem.getPrecoBase());
+        destino.setAtivo(origem.getAtivo());
+        log.trace("Campos do serviço atualizados em memória.");
+    }
+
+    public void atualizarEtapa(Servico destino, Servico origem) {
+        destino.setEtapa(etapaService.buscarPorTipoAndEtapa("PEDIDO", origem.getEtapa().getNome()));
+    }
+
+    public Servico editar(Servico origem, Integer id) {
+        Servico destino = this.buscarPorId(id);
+        this.atualizarCampos(destino, origem);
+        this.atualizarEtapa(destino, origem);
+        Servico servicoAtualizado = repository.save(destino);
+        log.info("Serviço com nome: {}  atualizado com sucesso!", servicoAtualizado.getNome());
+        return servicoAtualizado;
+    }
+
+    public void deletar(Integer id) {
+        Servico servicoParaDeletar = this.buscarPorId(id);
+        repository.deleteById(id);
+        String mensagem = String.format("Serviço ID %d (Nome: %s) deletado com sucesso.",
+                id, servicoParaDeletar.getNome());
+        logService.info(mensagem);
+    }
+}
